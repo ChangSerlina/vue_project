5256b44b6e482ed12d5b089cfbe21d95cab8aba9
榕軒 <serlina0504@gmail.com>
2025-09-16 00:40:07 +0800

    header

---
+++ portfolio-vue/src/components/Header.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="flex">
+        <!-- 左邊LOGO -->
+        <div class="col-2 logo">serlina</div>
+
+        <!-- 大螢幕選單 -->
+        <div class="col-8 menu" v-if="!isMobile">
+            <ul class="tabs">
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+
+        <!-- 右邊icon -->
+        <div class="col-2 icon" v-if="!isMobile">
+            <img src="https://img.icons8.com/?size=25&id=132&format=png&color=000000" alt="搜尋" />
+            <img src="https://img.icons8.com/?size=25&id=22396&format=png&color=000000" alt="使用者" />
+        </div>
+
+        <!-- 小螢幕漢堡icon -->
+        <div class="col-2 menu-mobile" v-if="isMobile">
+            <img src="https://img.icons8.com/?size=50&id=78290&format=png&color=000000" alt="menu" class="menu-icon"
+                @click="toggleMenu" />
+            <!-- 下拉選單 -->
+            <ul v-if="showMenu" class="dropdown">
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+                <img src="https://img.icons8.com/?size=25&id=132&format=png&color=000000" alt="搜尋" />
+                <img src="https://img.icons8.com/?size=25&id=22396&format=png&color=000000" alt="使用者" />
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const isMobile = ref(window.innerWidth < 768);
+const showMenu = ref(false);
+
+const checkScreen = () => {
+    isMobile.value = window.innerWidth < 768;
+    if (!isMobile.value) showMenu.value = false; // 回到大螢幕自動關閉選單
+};
+
+const toggleMenu = () => {
+    showMenu.value = !showMenu.value;
+};
+
+onMounted(() => {
+    window.addEventListener("resize", checkScreen);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener("resize", checkScreen);
+});
+</script>
+
+<style>
+.flex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.tabs {
+    display: flex;
+    justify-content: space-around;
+    gap: 20px;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.icon {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.icon img {
+    padding: 0 10px;
+    cursor: pointer;
+}
+
+/* 小螢幕隱藏大選單 */
+@media (max-width: 768px) {
+    .menu {
+        display: none;
+    }
+
+    .menu-icon {
+        cursor: pointer;
+        width: 30px;
+        height: 30px;
+    }
+
+    .dropdown {
+        text-align: center;
+        /* 讓裡面文字與圖片都置中 */
+        position: absolute;
+        top: 60px;
+        /* header 高度 */
+        left: 0;
+        right: 0;
+        background: white;
+        list-style: none;
+        margin: 0;
+        padding: 10px 0;
+        border-top: 1px solid #ccc;
+    }
+
+    .dropdown li {
+        padding: 10px;
+        text-align: center;
+    }
+
+    .dropdown li:hover {
+        background: #f2f2f2;
+    }
+
+    .dropdown img {
+        padding: 10px;
+        cursor: pointer;
+    }
+}
+</style>
